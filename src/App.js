68ef6fd73bc0aaa563f794d9f57f50436a53c383
pauldoody <pdoody@gmu.edu>
2020-04-07 19:40:42 -0400

    yeah

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import Fetcher from './Fetcher';
 import Hooks from './Hooks';
 
+import Skeleton from '@material-ui/lab/Skeleton';
+
 function App() {
   return (
     <div className="App">
       <Hooks/>
+	  
+	  <Skeleton variant="text" />
+      <Skeleton variant="circle" width={40} height={40} />
+      <Skeleton variant="rect" width={210} height={118} />
+	  
     </div>
   );
 }

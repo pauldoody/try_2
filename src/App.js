abeab012dbf1a458e13744c78f60bc50326702aa
pauldoody <pdoody@gmu.edu>
2020-04-13 17:09:22 -0400

    please

--- src/App.js
+++ src/App.js
@@ -8,22 +8,25 @@ import Skeleton from '@material-ui/lab/Skeleton';
 function App() {
   return (
     <div className="App">
-      <Hooks/>
+      <Hooks  />
 	  
-	  <Skeleton variant="text" />
-      <Skeleton variant="circle" width={40} height={40} />
-      <Skeleton variant="rect" width={210} height={118} />
+	  
 	  
     </div>
   );
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Fetcher url={'https://randomuser.me/api/'}/>
-//     </div>
-//   );
-// }
+/*<Skeleton variant="text" />
+      <Skeleton variant="circle" width={40} height={40} />
+      <Skeleton variant="rect" width={210} height={118} />*/
+
+/*//https://randomuser.me/api/*/
+ /*function App() {
+   return (
+     <div className="App">
+       <Fetcher url={'https://pauldoodyparticipation.herokuapp.com/echo/'}/>  
+     </div>
+  );
+ }*/
 
 export default App;
--- src/Hooks.js
+++ src/Hooks.js
@@ -1,20 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
+import Skeleton from '@material-ui/lab/Skeleton';
 
 export default function Hooks(props) {
     const { classes, url } = props;
     let [clicks, setClicks] = useState(0);
+	let [skelHeight] = useState(40);
+	
+	//added
+	const [seconds, setSeconds] = useState(0);
 
+	useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+	}, []);
+	//
+	
     const doSomething = function (event) {
         console.log(event.currentTarget.getAttribute('data-something'));
         setClicks(clicks + 1);
     }
+	
+	if(seconds < 2){
+	
+	//default return
     return (
         <React.Fragment>
-            <p>click count:{clicks}</p>
+            <p>click count: {clicks} , {seconds}</p>
             <button onClick={doSomething} data-something="Default">
                 Default</button>
+				<p></p>
+			<Skeleton variant="rect" width={2000} height={skelHeight} align="justify" />
         </React.Fragment>
 
     );
+	
+	}
+	else
+	{
+		return (
+        <React.Fragment>
+            <p>click count: {clicks} , {seconds}</p>
+            <button onClick={doSomething} data-something="Default">
+                Default</button>
+				<p>LOADED CONTENT</p>
+        </React.Fragment>
+
+    );
+	}
 }
